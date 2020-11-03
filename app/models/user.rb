df6f598cbf9fd9a23d52b02d7b16209815ecf4b7
vanak <ven2kaze@gmail.com>
2020-11-03 17:23:33 +0100

    first functionalities ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   after_create :welcome_send
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, 
-    presence:true, 
-    uniqueness: true, 
-    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-  validates :description, presence: true
 
   has_many :attendances
   has_many :events, through: :attendances
